696657c7b3843deeaca41242945f8700ed50907a
Stefan Gerrits <stefangerrits2000@gmail.com>
2019-10-25 13:40:34 +0200

    add simple routing

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,23 @@
+<!-- App.svelte -->
 <script>
-	import { onMount } from 'svelte';
 	import NavMenu from './shared/NavMenu.svelte'
 	import Dolls from './queries/Dolls.svelte'
 	import Cars from './queries/Cars.svelte'
 	import Weapons from './queries/Weapons.svelte'
 	import BoardGames from './queries/BoardGames.svelte'
+	import Home from './shared/Home.svelte'
+	import { Router, Route } from "svelte-routing";
+	export let url = "";
 </script>
 
-<NavMenu/>
-<Weapons/>
+<Router url="{url}">
+	<NavMenu/>
+
+  	<div>
+		<Route path="/" component="{Home}" />
+		<Route path="/poppen" component="{Dolls}" />
+		<Route path="/wapens" component="{Weapons}" />
+		<Route path="/bordspellen" component="{BoardGames}"/>
+		<Route path="/autos" component="{Cars}"/>
+  	</div>
+</Router>
--- src/shared/NavMenu.svelte
+++ src/shared/NavMenu.svelte
@@ -1,16 +1,20 @@
 <script>
     import NavItem from './NavItem.svelte'
+    import { Link } from "svelte-routing";
 </script>
 
 <style>
-
+	ul {
+  		list-style-type: none;
+	}
 </style>
 
 <nav>
-    <ol>
-        <NavItem text="Poppen" route="/poppen"/>
-        <NavItem text="Auto's" route="/auto's"/>
-        <NavItem text="Bordspellen" route="/bordspellen"/>
-        <NavItem text="Wapens" route="/wapens"/>
-    </ol>
+    <ul>
+        <Link to="/">Home</Link>
+        <Link to="/poppen">Poppen</Link>
+        <Link to="/wapens">Wapens</Link>
+        <Link to="/bordspellen">Bordspellen</Link>
+        <Link to="/autos">Auto's</Link>
+    </ul>
 </nav>
